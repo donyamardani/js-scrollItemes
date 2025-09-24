b485647a14deca99ceb6263fb796fad6f91d1c29
donyamardani <zmardani0@gmail.com>
2025-09-24 14:14:09 +0330

    add js code
    create js code for scroll items

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-'use strict'
+
+"use scrict";
+
+const itemesEl=document.querySelectorAll('.itemes')
+
+const scorollFunc=()=>{
+    const triggerBottom=window.innerHeight*.8
+    for(let i=0;i<itemesEl.length;i++){
+        const top=itemesEl[i].getBoundingClientRect().top
+        if(top<triggerBottom){
+            itemesEl[i].classList.add('show')
+        }else{itemesEl[i].classList.remove('show')}
+    }
+}
+window.addEventListener('scroll',scorollFunc)
+scorollFunc()
